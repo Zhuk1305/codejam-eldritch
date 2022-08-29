cb0100089be021202bbed2608c0a8b2a296d3573
Zhuk1305 <vaneczhuk@gmail.com>
2022-08-29 10:07:11 +0300

    feat: add play for normal level

---
+++ data/ancients.js
@@ -0,0 +1,86 @@
+// import Ancients from "../assets/Ancients/index.js";
+
+const ancientsData = [
+  {
+    id: "azathoth",
+    name: "azathoth",
+    // cardFace: Ancients.azathoth,
+    firstStage: {
+      green: 1,
+      blue: 1,
+      brown: 2,
+    },
+    secondStage: {
+      green: 2,
+      blue: 1,
+      brown: 3,
+    },
+    thirdStage: {
+      green: 2,
+      blue: 0,
+      brown: 4,
+    },
+  },
+  {
+    id: "cthulhu",
+    name: "cthulhu",
+    // cardFace: Ancients.cthulhu,
+    firstStage: {
+      green: 0,
+      blue: 2,
+      brown: 2,
+    },
+    secondStage: {
+      green: 1,
+      blue: 0,
+      brown: 3,
+    },
+    thirdStage: {
+      green: 3,
+      blue: 0,
+      brown: 4,
+    },
+  },
+  {
+    id: "iogSothoth",
+    name: "iogSothoth",
+    // cardFace: Ancients.iogSothoth,
+    firstStage: {
+      green: 0,
+      blue: 1,
+      brown: 2,
+    },
+    secondStage: {
+      green: 2,
+      blue: 1,
+      brown: 3,
+    },
+    thirdStage: {
+      green: 3,
+      blue: 0,
+      brown: 4,
+    },
+  },
+  {
+    id: "shubNiggurath",
+    name: "shubNiggurath",
+    // cardFace: Ancients.shubNiggurath,
+    firstStage: {
+      green: 1,
+      blue: 1,
+      brown: 2,
+    },
+    secondStage: {
+      green: 3,
+      blue: 1,
+      brown: 2,
+    },
+    thirdStage: {
+      green: 2,
+      blue: 0,
+      brown: 4,
+    },
+  },
+];
+
+export default ancientsData;
